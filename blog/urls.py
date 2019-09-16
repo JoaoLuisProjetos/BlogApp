3f53d5e6fac39771e5139bb123d772a5e1a07b22
Joao Luis <joaoluis.pasquarelli@yahoo.com>
2019-09-16 13:41:18 -0300

    Adicionada funcionalidade Edit Posts

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns =[
     # localhost/post/postID
     path('post/<int:pk>', views.post_detail, name='post_detail'),
 
+    path('post/new/', views.post_new, name='post_new'),
 
-    path('post/new/', views.post_new, name='post_new')
-
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', stuff_for_frontend)
 
 def post_new(request):
+    print(request.method)
     # POST
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,5 +30,21 @@ def post_new(request):
     else:
         form = PostForm()
         stuff_for_frontend = {'form': form}
+    return render(request, 'blog/post_edit.html', stuff_for_frontend)
 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+        stuff_for_frontend = {'form': form}
     return render(request, 'blog/post_edit.html', stuff_for_frontend)
